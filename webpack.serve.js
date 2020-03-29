560b1206d0c79c6029a6a119b3ca3ca47360cb8b
julian dondero <jdondero@zetti.com.ar>
2020-03-29 15:19:27 -0300

    set index settings

--- webpack.serve.js
+++ webpack.serve.js
@@ -1,18 +1,18 @@
 /* global module */
-const merge = require("webpack-merge");
-const common = require("./webpack.common.js");
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
-    mode: "development",
-    devtool: "inline-source-map",
+    mode: 'development',
+    devtool: 'inline-source-map',
     devServer: {
-        contentBase: "./"
+        contentBase: './'
     },
     plugins: [
         new HTMLWebpackPlugin({
-            title: 'Converse.js Dev',
-            template: 'webpack.html'
+            title: 'go5-chat',
+            template: 'index.html'
         })
-    ],
+    ]
 });
